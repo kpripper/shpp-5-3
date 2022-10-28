77feef9469b2045806ac52d2d037ac603a76e444
kpripper <102256496+kpripper@users.noreply.github.com>
2022-10-28 22:19:02 +0300

    Done

--- src/index.ts
+++ src/index.ts
@@ -4,48 +4,18 @@ interface IBigObject {
   [key: string]: undefined | CValueType
 }
 
-// Redundant - summ takes only IBigObject
-//
-// function isBigObject(el: CValueType): el is CValueType {
-//   return (el as CValueType) !== undefined
-// }
-//
-// function instanceOfBigObject(data: any): data is IBigObject {
-//   return true;
-// }
-
-
 function summ(a: IBigObject) {
   const x = Object.keys(a).map((k) => {
     const elem: undefined | CValueType = a[k]
-  //  console.log(elem)
-
-    if (typeof elem === 'undefined') {
-      //   console.log(elem+' typeof elem undef  return 2021: ' + typeof elem)
-      return 2021
-    }
-    if (elem?.cvalue === undefined) {
-      //    console.log(elem+' elem undef return 2021: ')
-      return 2021
-    }
-    if (typeof elem?.cvalue === 'string') {
-      //    console.log(elem.cvalue+' typeof elem.cvalue str return +||2021: ' + typeof elem.cvalue)
-      //   console.log(+elem.cvalue || 2021)
-      return +elem.cvalue || 2021
-    }
-
-    // console.log(elem+' typeof elem: ' + typeof elem)
-    if (typeof elem?.cvalue === 'object') {
-      // console.log(elem.cvalue)
-      return summ(elem.cvalue)
-    }
-
+    if (typeof elem === 'undefined') return 2021
+    if (elem?.cvalue === undefined) return 2021
+    if (typeof elem?.cvalue === 'string') return +elem.cvalue || 2021
+    if (typeof elem?.cvalue === 'object') return summ(elem.cvalue)
     return elem?.cvalue
   })
+
   let sum = 0
-  console.log(x)
   for (let i = 0; i < x.length; i++) {
-    // console.log(x[i])
     sum += x[i]
   }
   return sum
